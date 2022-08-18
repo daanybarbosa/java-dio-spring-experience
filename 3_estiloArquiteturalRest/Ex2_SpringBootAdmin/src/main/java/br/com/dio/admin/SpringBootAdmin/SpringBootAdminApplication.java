8ee89ea9fc1d88169b94f345b139168d8f4a925e
Daniele Barbosa <daanybsilva@gmail.com>
2022-08-18 11:22:40 -0300

    Metricas com Actuator

--- 3_estiloArquiteturalRest/Ex2_SpringBootAdmin/src/main/java/br/com/dio/admin/SpringBootAdmin/SpringBootAdminApplication.java
+++ 3_estiloArquiteturalRest/Ex2_SpringBootAdmin/src/main/java/br/com/dio/admin/SpringBootAdmin/SpringBootAdminApplication.java
@@ -1,9 +1,13 @@
 package br.com.dio.admin.SpringBootAdmin;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
 public class SpringBootAdminApplication {
 
 	public static void main(String[] args) {
