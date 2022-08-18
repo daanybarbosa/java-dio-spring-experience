f1a80f09d27e40e877c0a829794f604e7ad10cf1
Daniele Barbosa <daanybsilva@gmail.com>
2022-08-18 12:09:34 -0300

    Health Check

---
+++ 3_estiloArquiteturalRest/Ex1_RestSpring/src/main/java/br/com/dio/config/check/HealthCheck.java
@@ -0,0 +1,27 @@
+package br.com.dio.config.check;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+import java.net.InetAddress;
+
+/**
+ * HealthCheck customizado
+ */
+
+@Component
+public class HealthCheck implements HealthIndicator {
+
+    @Override
+    public Health health(){
+        try {
+            InetAddress address = InetAddress.getByName("localhost");
+            if(address.isReachable(10000))
+                return Health.up().build();
+        } catch (Exception e){
+            return Health.down().withDetail("Motivo ", e.getMessage()).build();
+        }
+        return Health.down().withDetail("Motivo", "Motivo desconhecido").build();
+    }
+}
