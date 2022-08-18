5e46d6b6abce12947c5853469bd6e88463b0fbc5
Daniele Barbosa <daanybsilva@gmail.com>
2022-08-18 14:28:10 -0300

    Teste unitario JUnit

---
+++ 3_estiloArquiteturalRest/Ex1_RestSpring/src/test/java/br/com/dio/ProdutoTest.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ProdutoTest {
+
+    @Test
+    public void testeFalso(){
+
+        assertTrue(true);
+    }
+}
